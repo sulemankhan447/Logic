467e5d942a7d12c65486f63347c11d830c3ae6b6
sulemankhan447 <sulemankhan447@gmail.com>
2018-11-01 12:00:37 +0530

    Optimized Code for Increasing Order Problem in Java

--- IncreasingOrder.java
+++ IncreasingOrder.java
@@ -1,8 +1,30 @@
 // url for problem defination 
 // https://www.geeksforgeeks.org/minimum-increment-operations-to-make-the-array-in-increasing-order/
-
+import java.util.Scanner;
 public class IncreasingOrder {
 	public static void main(String args[]){
-		
+		Scanner sc = new Scanner(System.in);
+		int[] array = new int[4];
+		int i,inc = 2,steps=0;
+
+		System.out.println("Enter "+array.length+" Elements for Array");
+		for(i=0;i<array.length;i++)
+		{
+			array[i] = sc.nextInt();
+		}
+		for(i=1;i<array.length;i++)
+		{
+			if(array[i]<=array[i-1]){
+				array[i] = array[i-1]+inc;
+				steps++;
+			}
+		}
+		System.out.println("Output is ");
+		for(i=0;i<array.length;i++)
+		{
+			System.out.print(" "+array[i]);
+		}
+		System.out.println("\nNumber of Steps Required to Make it IncreasingOrder is "+steps);
 	}
 }
+
