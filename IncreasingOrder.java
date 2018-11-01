7161ffd807e39772af5b0d28d9b282d0d68086a5
sulemankhan447 <sulemankhan447@gmail.com>
2018-11-01 11:30:05 +0530

    Increasing Order Problem

---
+++ IncreasingOrder.java
@@ -0,0 +1,8 @@
+// url for problem defination 
+// https://www.geeksforgeeks.org/minimum-increment-operations-to-make-the-array-in-increasing-order/
+
+public class IncreasingOrder {
+	public static void main(String args[]){
+		
+	}
+}
