7e60994da0b99f4e2cdd3feffe43436770cadfe1
sulemankhan447 <sulemankhan447@gmail.com>
2018-11-09 18:47:48 +0530

    url added

--- PairSum.java
+++ PairSum.java
@@ -1,5 +1,7 @@
+//url for problem defination 
+//https://practice.geeksforgeeks.org/problems/pair-with-given-sum-in-a-sorted-array/0
 public class PairSum {
 	public static void main(String args[]){
-		
+
 	}
 }
